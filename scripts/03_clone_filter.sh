bfb519cba0699afeaac432955c10afdc84986b52
Pauline Owusu-Ansah <paulineowusu653@gmail.com>
2025-08-13 16:54:07 -0400

    Create 03_clone_filter.sh
    Clonefilter with STACKS to remove PCR Duplicates:

---
+++ scripts/03_clone_filter.sh
@@ -0,0 +1,6 @@
+clone_filter \
+-f /shared/jezkovt_shared/Genomic_Data/RADseq/Ambystoma_RADseq/AMBA_raw/trimmed/AMBA_4_trimmed.fastq.gz \
+-i gzfastq \
+-o /shared/jezkovt_shared/Genomic_Data/RADseq/Ambystoma_RADseq/AMBA_raw/clonefiltered/ \
+--inline_null \
+--oligo_len_1 6
